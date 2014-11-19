3624f3e108ec44819694b23e78551c68bb781c8d
Peter Hughes <peterhughes.dev@gmail.com>
2014-11-19 15:55:14 +0000

    Add initial source

---
+++ saywhen.js
@@ -0,0 +1,107 @@
+var handlers = [];
+
+function MatcherSet(matchers) {
+    matchers = matchers || [];
+    var responses = [];
+
+    this.matches = function(args) {
+        if (matchers.length !== args.length) return false;
+
+        for (var i = 0; i < args.length; ++i) {
+            var matcher = matchers[i], result = false;
+            
+            if (matcher instanceof Function) {
+                result = matcher(args[i]);
+            } else if (matcher.jasmineMatches !== undefined) {
+                result = matcher.jasmineMatches(args[i]);
+            } else  {
+                result = matchers[i] === args[i];
+            }
+
+            if (!result) return false;
+        }
+
+        return true;
+    };
+
+    this.apply = function(args) {
+        var res = responses.length > 1 ? responses.shift() : responses[0];
+
+        if (res) {
+            return res.apply(res, args);
+        }
+    };
+
+    this.addResponse = function(fn) {
+        responses.push(fn);
+    };
+}
+
+function CallHandler(spy) {
+    var matchers = [];
+
+    spy.and.callFake(function() {
+        var args = Array.prototype.slice.call(arguments, 0);
+
+        for (var i = 0; i < matchers.length; ++i) {
+            var set = matchers[i];
+            
+            if (set.matches(args)) {
+                return set.apply(args);
+            }
+        }
+    });
+
+    this.isCalledWith = function() {
+        var args = Array.prototype.slice.call(arguments, 0);
+
+        var matcher = new MatcherSet(args);
+        matchers.push(matcher);
+
+        return {
+            then : function(fn) {
+                matcher.addResponse(fn);
+                return this;
+            },
+            thenReturn : function(val) {
+                matcher.addResponse(function() {
+                    return val;
+                });
+
+                return this;
+            },
+            thenThrow : function(err) {
+                matcher.addResponse(function() {
+                    throw err;
+                });
+
+                return this;
+            }
+        };
+    };
+}
+
+function when(spy) {
+    for (var i = 0; i < handlers.length; ++i) {
+        if (handlers[i].spy === spy) {
+            return handlers[i].handler;
+        }
+    }
+    
+    var handler = new CallHandler(spy);
+    
+    handlers.push({
+        'spy' : spy,
+        'handler' : handler
+    });
+
+    return handler;
+}
+
+when.is = function(val) {
+    return function(arg) {
+        return val == arg;
+    };
+};
+
+module.exports = when;
---
+++ spec/testSpec.js
@@ -0,0 +1,161 @@
+var when = require('../saywhen');
+
+describe("a test spy", function() {
+    var spy;
+
+    beforeEach(function() {
+        spy = jasmine.createSpy('foo');
+    });
+
+    it("calls a response", function() {
+        when(spy).isCalledWith(jasmine.any(String), jasmine.any(Object)).then(function(str, obj) {
+            return obj[str];
+        });
+
+        var result = spy("foo", { foo : "bar" });
+        
+        expect(result).toBe("bar");
+    });
+
+    it("doesn't call response with wrong type", function() {
+        when(spy).isCalledWith(jasmine.any(String)).then(function() {
+            return true;
+        });
+
+        expect(spy(123)).toBe(undefined);
+    });
+
+    it("can assign response value", function() {
+        when(spy).isCalledWith(jasmine.any(String)).thenReturn("string");
+
+        expect(spy("foo")).toBe("string");
+        expect(spy(123)).toBe(undefined);
+    });
+
+    it("can use function as matcher", function() {
+        var matcher = function(v) {
+            return v === 123;
+        };
+
+        when(spy).isCalledWith(matcher).thenReturn(456);
+
+        expect(spy(123)).toBe(456);
+        expect(spy(456)).toBe(undefined);
+    });
+
+    it("works with value matching", function() {
+        when(spy).isCalledWith("hello").thenReturn("world");
+
+        expect(spy("hello")).toBe("world");
+        expect(spy("goodbye")).toBe(undefined);
+    });
+
+    it("only does strict value matching", function() {
+        when(spy).isCalledWith(123).thenReturn(456);
+
+        expect(spy(123)).toBe(456);
+        expect(spy("123")).toBe(undefined);
+    });
+
+    it("can use provided loose equals matcher", function() {
+        when(spy).isCalledWith(when.is(123)).thenReturn(456);
+
+        expect(spy(123)).toBe(456);
+        expect(spy("123")).toBe(456);
+        expect(spy("something else")).toBe(undefined);
+    });
+
+    it("uses complex object matching", function() {
+        when(spy).isCalledWith(jasmine.objectContaining({
+            foo : 123
+        })).thenReturn(456);
+
+        expect(spy({
+            foo : 123,
+            bar : "hello"
+        })).toBe(456);
+
+        expect(spy({
+            foo : 789
+        })).toBe(undefined);
+
+        expect(spy({
+            bar : "hello"
+        })).toBe(undefined);
+    });
+
+    it("can throw errors", function() {
+        when(spy).isCalledWith(123).thenThrow(new Error("foo"));
+
+        expect(function() {
+            spy(123);
+        }).toThrow();
+
+        expect(spy(456)).toBe(undefined);
+    });
+
+    it("can assign multiple responses", function() {
+        when(spy).isCalledWith(jasmine.any(String))
+            .then(function() {
+                return 1;
+            })
+            .then(function() {
+                return 2;
+            })
+            .then(function() {
+                return 3;
+            });
+
+        expect(spy("first")).toBe(1);
+        expect(spy("second")).toBe(2);
+        expect(spy("third")).toBe(3);
+    });
+
+    it("can assign responses and return values", function() {
+        when(spy).isCalledWith(jasmine.any(String))
+            .then(function() {
+                return 1;
+            })
+            .thenReturn(2);
+
+        expect(spy("first")).toBe(1);
+        expect(spy("second")).toBe(2);
+    });
+
+    it("calls single response multiple times", function() {
+        when(spy).isCalledWith(jasmine.any(String)).then(function() {
+            return 1;
+        });
+
+        expect(spy("foo")).toBe(1);
+        expect(spy("foo")).toBe(1);
+        expect(spy("bar")).toBe(1);
+    });
+
+    it("calls all responses and repeats last one", function() {
+        when(spy).isCalledWith(jasmine.any(String))
+            .then(function() {
+                return 1;
+            })
+            .then(function() {
+                return 2;
+            });
+
+        expect(spy("first")).toBe(1);
+        expect(spy("second")).toBe(2);
+        expect(spy("third")).toBe(2);
+    });
+
+    it("can assign multiple argument matchers", function() {
+        when(spy).isCalledWith(jasmine.any(String)).then(function() {
+            return "string";
+        });
+
+        when(spy).isCalledWith(jasmine.any(Number)).then(function() {
+            return "number";
+        });
+
+        expect(spy("foo")).toBe("string");
+        expect(spy(123)).toBe("number");
+    });
+});
